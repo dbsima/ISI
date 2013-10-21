a4279c0142217c196b73eb250cee8d37f782d4b2
Andrei Gliga <otzy_007@yahoo.com>
2013-10-21 18:20:22 +0300

    fisiere proiect

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TimesheetManagement::Application.config.secret_key_base = '208c67029d11c9ed335ec9ba51cd60a0700f697584ff7ce86aa5d147b97dc9ade97abc01ad710bf892aa741108e9409183b70c127f8f0723b86e04a48d3aacf8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TimesheetManagement::Application.config.session_store :cookie_store, key: '_TimesheetManagement_session'
