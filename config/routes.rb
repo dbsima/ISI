f04b791599dd373db95c16a654d467a394a28462
Andrei Gliga <otzy_007@yahoo.com>
2013-10-26 21:48:39 +0300

    controller de test pt privilegii

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 TimesheetManagement::Application.routes.draw do
+  get "dashboard/index"
   devise_for :users
+  
+  resources :dashboard
+  
+  root 'dashboard#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
