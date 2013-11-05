9f752a010699ef871bdffc4ebf94280bfe8feda9
Andrei Gliga <otzy_007@yahoo.com>
2013-11-05 23:04:59 +0200

    client model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131105210224) do
+ActiveRecord::Schema.define(version: 20131105210428) do
+
+  create_table "clients", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "departments", force: true do |t|
     t.string   "name"
