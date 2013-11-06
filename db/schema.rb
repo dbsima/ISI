e0f660a5bcefd8d9c35359f518860ac94e5750b2
Andrei Gliga <otzy_007@yahoo.com>
2013-11-06 14:10:20 +0200

    db schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131105210428) do
+ActiveRecord::Schema.define(version: 20131105210530) do
 
   create_table "clients", force: true do |t|
     t.string   "name"
@@ -27,6 +27,13 @@ ActiveRecord::Schema.define(version: 20131105210428) do
   end
 
   create_table "divisions", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "projects", force: true do |t|
+    t.integer  "client_id"
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
