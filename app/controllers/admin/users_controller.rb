6fdeb2df805289a9f4845b654364744528346370
Andrei Gliga <otzy_007@yahoo.com>
2013-11-11 11:35:54 +0200

    adaugare utilizator

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -8,10 +8,19 @@ class Admin::UsersController < Admin::AdminController
   end
 
   def create
-    p = params.require 'user'
-    p p
-    User.create! name: p
-    redirect_to :index
+    par = params.require('user').permit(:first_name, :last_name, :email, :password, :internal_id, :division, :department, :role)
+    p par
+    User.create!(
+       first_name: par[:first_name], 
+       last_name: par[:last_name], 
+       email: par[:email], 
+       password: par[:password],
+       internal_id: par[:internal_id],
+       department_id: par[:department],
+       roles_mask: par[:role]
+   )
+#     User.create! name: p
+    redirect_to :admin_users
   end
 
   def edit
