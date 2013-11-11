2d574a97c3e1a4f0de867412b577de8c82cc43e9
Andrei Gliga <otzy_007@yahoo.com>
2013-11-11 12:39:42 +0200

    managementul diviziilr

--- app/controllers/admin/divisions_controller.rb
+++ app/controllers/admin/divisions_controller.rb
@@ -5,8 +5,32 @@ class Admin::DivisionsController < Admin::AdminController
 
   def new
   end
+  
+  def create
+     begin
+	Division.create params.require('division').permit('name')
+	redirect_to :admin_divisions, :alert => 'Division created'
+     rescue ActionController::ParameterMissing
+	redirect_to :new_admin_division, :alert => 'Please input a division name'
+     end
+  end
 
   def edit
      @division = Division.find params.require('id')
+  end
+  
+  def update
+     begin
+	Division.find(params.require('id')).update_attributes params.require('division').permit('name')
+	redirect_to :admin_divisions, :alert => 'Division updated'
+     rescue ActionController::ParameterMissing
+	redirect_to :new_admin_division, :alert => 'Please input a division name'
+     end 
+  end
+  
+  def destroy
+     Division.find(params.require('id')).destroy
+     
+     redirect_to :admin_divisions, :alert => 'Division removed'
   end
 end
