303b653f34a65be4529414aadc97b14ab80b3a09
Andrei Gliga <otzy_007@yahoo.com>
2013-12-14 17:21:50 +0200

    afiseaza statusul timesheetului

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,15 @@
 class DashboardController < ApplicationController
   def index
     @user = current_user
+    begin
+       month = params.require :month
+       year = params.require :year
+       @ms = @user.monthly_sheets.find_by_month Date.new(year.to_i, month.to_i, 1)
+       @this_month = true if Date.new(year.to_i, month.to_i, 1) == Date.today.at_beginning_of_month
+    rescue ActionController::ParameterMissing
+       @this_month = true
+       @ms = @user.monthly_sheets.find_by_month Date.today.at_beginning_of_month
+    end
     
     if current_user.has_role? :manager
         @role = 'manager'
