5c402432e743236ca76050fca69182a769ecaa6b
Andrei Gliga <otzy_007@yahoo.com>
2013-12-14 21:53:53 +0200

    adaugare/stergere/modificare proiect

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,8 +4,28 @@ class ProjectsController < DeptChiefController
   end
 
   def edit
+     @project = Project.find params.require(:id)
   end
-
+  
+  def update
+     project = Project.find params.require(:id)
+     project.update_attributes params.require(:project).permit(:name, :client_id)
+     
+     redirect_to :projects, :alert => 'Proiectul a fost modificat'
+  end
+  
   def new
+  end
+  
+  def create
+     Project.create params.require(:project).permit(:name, :client_id)
+     
+     redirect_to :projects, :alert => 'Proiectul a fost adaugat'
+  end
+  
+  def destroy
+     Project.find(params.require(:id)).destroy
+     
+     redirect_to :projects, :alert => 'Proiectul a fost sters'
   end
 end
