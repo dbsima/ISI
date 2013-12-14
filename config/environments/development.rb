3ef73478e761ace9ec36bb8212c74b05a02c888b
Andrei Gliga <otzy_007@yahoo.com>
2013-12-14 14:58:32 +0200

    configuratii pt mail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,18 @@ TimesheetManagement::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.smtp_settings = {
+      :address => "cloudients.com",
+      :port => 465,
+      :domain => 'cloudients.com',
+      :user_name => 'isi@cloudients.com',
+      :password => 'uruhNSFG',
+      :authentication => 'plain',
+      :enable_starttls_auto => false,
+      :ssl => true
+  }
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,17 @@ TimesheetManagement::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.smtp_settings = {
+      :address => "cloudients.com",
+      :port => 465,
+      :domain => 'cloudients.com',
+      :user_name => 'isi@cloudients.com',
+      :password => 'uruhNSFG',
+      :authentication => 'plain',
+      :enable_starttls_auto => false,
+      :ssl => true
+  }
 end
