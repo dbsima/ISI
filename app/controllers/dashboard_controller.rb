10ae41345764e64bed4f56ca883cd6fc4fecc870
Andrei Gliga <otzy_007@yahoo.com>
2013-12-14 16:43:31 +0200

    afiseaza taskurile utilizatorului nu toate

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,7 @@
 class DashboardController < ApplicationController
   def index
+    @user = current_user
+    
     if current_user.has_role? :manager
         @role = 'manager'
     elsif current_user.has_role? :employee
@@ -8,7 +10,8 @@ class DashboardController < ApplicationController
         @role = 'dept_chief'
     end
     
-    @timesheets = Task.all
+    p @user
+    @timesheets = @user.tasks
   end
 
   def new
