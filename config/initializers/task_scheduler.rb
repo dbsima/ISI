a534227bdbc0e1cc3ced1816c0e9c2daed3e0c0c
Andrei Gliga <otzy_007@yahoo.com>
2013-12-14 16:58:42 +0200

    creeaza fisele lunare pt utilizatori

--- config/initializers/task_scheduler.rb
+++ config/initializers/task_scheduler.rb
@@ -2,6 +2,10 @@ require 'rufus/scheduler'
 
 scheduler = Rufus::Scheduler.new
 
-scheduler.cron '22 19 1 * *' do
-   
+scheduler.every '1m' do
+   p 'Starting monthly_sheets creation'
+   this_month = Date.today.at_beginning_of_month
+   User.all.each do |u|
+      u.monthly_sheets.create(month: this_month, status: 'open') unless u.monthly_sheets.find_by_month(this_month)
+   end
 end
