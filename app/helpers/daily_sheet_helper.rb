7ae39992089917862495422bc8758812f749f082
Andrei Gliga <otzy_007@yahoo.com>
2013-12-15 16:58:01 +0200

    marcheaza orele extra

--- app/helpers/daily_sheet_helper.rb
+++ app/helpers/daily_sheet_helper.rb
@@ -1,2 +1,11 @@
 module DailySheetHelper
+   def extra_hours(t)
+      if @acc < 8
+	 res = 'normal_hours'
+      else
+	 res = 'extra_hours'
+      end
+      @acc += t.number_of_hours
+      res
+   end
 end
