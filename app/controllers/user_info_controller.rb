0a68fdd88858795d244a28c527f08a1e248824b6
Andrei Gliga <otzy_007@yahoo.com>
2013-12-15 16:39:30 +0200

    redirect pt admin

--- app/controllers/user_info_controller.rb
+++ app/controllers/user_info_controller.rb
@@ -1,5 +1,10 @@
 class UserInfoController < ApplicationController
   def index
+	if current_user.has_role? :admin
+	   redirect_to :admin_users
+	   
+	   return
+	end
 	@user = current_user
 	if @user.department
 		division = Division.find(@user.department.division_id)
