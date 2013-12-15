437dadda18bedb7046da7bc12964cb6b4ec91157
Andrei Gliga <otzy_007@yahoo.com>
2013-12-15 17:12:48 +0200

    doar angajatul are buton de trimite

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,6 +1,6 @@
 module DashboardHelper
    def submit_timesheet_button
-     if @ms.status == 'deschis' && @ms.user == current_user
+     if @ms.status == 'deschis' && @ms.user == current_user && @ms.user.roles_mask > 15
 	button_to 'Trimite', dashboard_submit_path(@ms.id)
      elsif @ms.status == 'respins' && @ms.user == current_user
 	button_to 'Redeschide', dashboard_reopen_path(@ms.id)
