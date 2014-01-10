36a9557ed3e559c2c62159a078190efe62fbb6dd
Andrei Gliga <otzy_007@yahoo.com>
2014-01-10 22:16:03 +0200

    raport pt toate proiectele

--- app/controllers/project_report_controller.rb
+++ app/controllers/project_report_controller.rb
@@ -15,5 +15,19 @@ class ProjectReportController < ApplicationController
       @employees[u_id] = tasks.where(user_id: u_id).inject(0)  {|acc, x| acc + x.number_of_hours}
     end
   end
-  p @employees
+
+  def index
+    begin
+      pars = params.require(:report_date).permit :date_start, :date_finish
+    rescue ActionController::ParameterMissing => e
+
+    end
+
+    @projects = {}
+    tasks = current_user.department.tasks
+
+    tasks.pluck(:project_id).uniq.select { |x| x }.each do |p_id|
+      @projects[Project.find(p_id).name] = tasks.where(project_id: p_id).inject(0)  {|acc, x| acc + x.number_of_hours}
+    end
+  end
 end
