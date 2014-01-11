4d550319bf02255a9400fdedde291206b0728814
Andrei Gliga <otzy_007@yahoo.com>
2014-01-11 20:37:40 +0200

    sortare la proiecte

--- app/controllers/project_report_controller.rb
+++ app/controllers/project_report_controller.rb
@@ -19,15 +19,25 @@ class ProjectReportController < ApplicationController
   def index
     begin
       pars = params.require(:report_date).permit :date_start, :date_finish
+
     rescue ActionController::ParameterMissing => e
 
     end
+    @sort = params.permit :sort
 
     @projects = {}
     tasks = current_user.department.tasks
 
     tasks.pluck(:project_id).uniq.select { |x| x }.each do |p_id|
       @projects[Project.find(p_id).name] = tasks.where(project_id: p_id).inject(0)  {|acc, x| acc + x.number_of_hours}
+    end
+
+    if @sort[:sort]
+      if @sort[:sort] == 'asc'
+          @projects = @projects.sort_by { |k, v| v }
+      else
+        @projects = @projects.sort_by { |k, v| -v }
+      end
     end
   end
 end
--- app/helpers/project_report_helper.rb
+++ app/helpers/project_report_helper.rb
@@ -2,4 +2,13 @@ module ProjectReportHelper
   def user_name(id)
     User.find(id).first_name + " " + User.find(id).last_name
   end
+
+  def sort_link
+    p @sort
+    if @sort && @sort[:sort] == 'asc'
+      project_report_index_path(:sort => 'desc')
+    else
+      project_report_index_path :sort => 'asc'
+    end
+  end
 end
