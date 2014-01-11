05eb0183435614e614cc288f8e8548f6bacc92fe
Andrei Gliga <otzy_007@yahoo.com>
2014-01-12 01:30:53 +0200

    # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # On branch master # Your branch is ahead of 'origin/master' by 1 commit. #   (use "git push" to publish your local commits) # # You are currently bisecting, started from branch '0876691'. #   (use "git bisect reset" to get back to the original branch) # # Untracked files: #   (use "git add <file>..." to include in what will be committed) # #	.idea/ #	.kdev4/ #	README.rdoc #	TimesheetManagement.kdev4 #	app/assets/javascripts/departments.js.coffee #	app/assets/javascripts/employees.js.coffee #	app/assets/javascripts/project_report.js.coffee #	app/assets/javascripts/tasks.js.coffee #	app/assets/javascripts/user_report.js.coffee #	app/assets/stylesheets/departments.css.scss #	app/assets/stylesheets/employees.css.scss #	app/assets/stylesheets/project_report.css.scss #	app/assets/stylesheets/tasks.css.scss #	app/assets/stylesheets/user_report.css.scss #	app/helpers/user_report_helper.rb #	development.sqlite3 #	public/README.md #	public/adisi/.gitignore #	public/components.html #	public/getting-started.html #	public/package.json #	public/template.html #	test/controllers/admin/departments_controller_test.rb #	test/controllers/admin/divisions_controller_controller_test.rb #	test/controllers/admin/divisions_controller_test.rb #	test/controllers/clients_controller_test.rb #	test/controllers/daily_sheet_controller_test.rb #	test/controllers/departments_controller_test.rb #	test/controllers/employees_controller_test.rb #	test/controllers/project_controller_test.rb #	test/controllers/project_report_controller_test.rb #	test/controllers/tasks_controller_test.rb #	test/controllers/user_report_controller_test.rb #	test/fixtures/vacantions.yml #	test/helpers/admin/departments_helper_test.rb #	test/helpers/admin/divisions_controller_helper_test.rb #	test/helpers/admin/divisions_helper_test.rb #	test/helpers/clients_helper_test.rb #	test/helpers/daily_sheet_helper_test.rb #	test/helpers/departments_helper_test.rb #	test/helpers/employees_helper_test.rb #	test/helpers/project_helper_test.rb #	test/helpers/project_report_helper_test.rb #	test/helpers/tasks_helper_test.rb #	test/helpers/user_report_helper_test.rb #	test/models/vacantion_test.rb #	users

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,8 +1,10 @@
 class ApiController < ApplicationController
+  skip_before_filter :verify_authenticity_token, :authenticate_user!
+
   respond_to :json
 
   def extra
-    @angajat = User.find_by_internal_id params.require :id
+    @employee = User.find_by_internal_id params.require :id
 
     begin
       @data_start = params.require :data_start
@@ -10,9 +12,29 @@ class ApiController < ApplicationController
     rescue ActionController::ParameterMissing => e
 
     end
+
+
   end
 
   def concedii
+    @employee = User.find_by_internal_id params.require :id
 
+    unless @employee
+      render json: {error: 'Angajatul nu a fost gasit'}, status: :not_acceptable
+      return
+    end
+
+    begin
+      @data_start = params.require :data_start
+      @data_final = params.require :data_final
+
+    rescue ActionController::ParameterMissing => e
+      @data_start = Date.new 0
+      @data_final = Date.today
+    end
+
+    @holidays = @employee.tasks.where(activity: 'vacanta', created_at: @data_start .. @data_final).inject(0)  {|acc, x| acc + x.number_of_hours}
+    @sick = @employee.tasks.where(activity: 'concediu medical', created_at: @data_start .. @data_final).inject(0)  {|acc, x| acc + x.number_of_hours}
+    render json: {holiday: @holidays, sick: @sick}
   end
 end
---
+++ client.rb
@@ -0,0 +1,36 @@
+require 'rest_client'
+require 'irb'
+require 'json'
+
+# Client pt api
+def extra(id_ang, data_start = nil, data_final = nil)
+  r = RestClient.post 'http://localhost:3000/api/extra', {id: id_ang, data_start: data_start, data_final: data_final}
+
+  if r.code == 200
+  else
+    p JSON.parse(r.to_s)[:error]
+  end
+end
+
+def concedii(id_ang, data_start = nil, data_final = nil)
+  r = RestClient.post 'http://localhost:3000/api/concedii', {id: id_ang, data_start: data_start, data_final: data_final}
+  if r.code == 200
+    json = JSON.parse r.to_s
+
+    p "Ore vacanta: #{json['holiday']}"
+    p "Ore concediu medical: #{json['sick']}"
+  else
+    p JSON.parse(r.to_s)[:error]
+  end
+  return
+end
+
+p 'Mod utilizare'
+p 'extra id_angajat data_start data_final'
+p 'concedii id_angajat data_start data_final'
+#IRB.conf[:PROMPT] = {}
+#IRB.conf[:PROMPT][:DEFAULT] = {}
+#IRB.conf[:PROMPT][:DEFAULT][:RETURN] = ""
+#IRB.conf[:IRB_NAME]="Client Timesheet"
+
+a=IRB.start
