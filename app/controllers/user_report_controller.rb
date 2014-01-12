3bd6d6f49de0037395d9c38c25413fe138144b36
Andrei Gliga <otzy_007@yahoo.com>
2014-01-12 22:39:00 +0200

    junk

--- app/controllers/user_report_controller.rb
+++ app/controllers/user_report_controller.rb
@@ -1,6 +1,7 @@
 class UserReportController < ApplicationController
   include Concerns::DateSelect
 
+  skip_before_filter :authenticate_user!
   before_filter :get_date_params, :only => [:show]
   def index
     begin
