2105ff08a8745403d75d02ab40864155857ddea9
Andrei Gliga <otzy_007@yahoo.com>
2014-01-12 14:21:34 +0200

    API

--- client.rb
+++ client.rb
@@ -7,6 +7,10 @@ def extra(id_ang, data_start = nil, data_final = nil)
   r = RestClient.post 'http://localhost:3000/api/extra', {id: id_ang, data_start: data_start, data_final: data_final}
 
   if r.code == 200
+    json = JSON.parse r.to_s
+
+    p "Ore normale: #{json['norm']}"
+    p "Ore extra: #{json['extra']}"
   else
     p JSON.parse(r.to_s)[:error]
   end
--- config/initializers/task_scheduler.rb
+++ config/initializers/task_scheduler.rb
@@ -2,17 +2,17 @@ require 'rufus/scheduler'
 
 scheduler = Rufus::Scheduler.new
 
-#scheduler.every '1m' do
-#   p 'Starting monthly_sheets creation'
-#   this_month = Date.today.at_beginning_of_month
-#   User.all.each do |u|
-#      u.monthly_sheets.create(month: this_month, status: 'deschis') unless u.monthly_sheets.find_by_month(this_month)
-#   end
-#end
-#
-#scheduler.cron '00 01 1 * *' do
-#   p 'Sending alerts for unsent timesheets'
-#   User.all.where('roles_mask >= 16').each do |u|
-#      TimesheetMailer.remember(u).deliver if u.monthly_sheets.order('month').last.status == 'open'
-#   end
-#end
+scheduler.every '1m' do
+   p 'Starting monthly_sheets creation'
+   this_month = Date.today.at_beginning_of_month
+   User.all.each do |u|
+      u.monthly_sheets.create(month: this_month, status: 'deschis') unless u.monthly_sheets.find_by_month(this_month)
+   end
+end
+
+scheduler.cron '00 01 1 * *' do
+   p 'Sending alerts for unsent timesheets'
+   User.all.where('roles_mask >= 16').each do |u|
+      TimesheetMailer.remember(u).deliver if u.monthly_sheets.order('month').last.status == 'open'
+   end
+end
